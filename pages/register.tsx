e6fde91dc17cf1091f4820cf39a5207e538ad469
JooKangsan <162934516+JooKangsan@users.noreply.github.com>
2024-06-14 01:21:39 +0900

    feat : 회원가입 페이지 html 및 pc버전 css작성

---
+++ pages/register.tsx
@@ -0,0 +1,62 @@
+import Image from "next/image";
+import styles from "@/styles/sign.module.css";
+import Link from "next/link";
+import Input from "@/components/sign/Input";
+import Label from "@/components/sign/Label";
+import Button from "@/components/sign/SignButton";
+import EasyLogin from "@/components/sign/EasyLogin";
+
+function register() {
+  return (
+    <div className={styles.container}>
+      <Link href="/">
+        <Image
+          src="/Img/logo/panda-logo_login.svg"
+          width={396}
+          height={132}
+          alt="판다마켓 로고"
+        />
+      </Link>
+      <form className={styles.form}>
+        <Label htmlFor="email">이메일</Label>
+        <Input
+          id="email"
+          name="email"
+          type="email"
+          placeholder="이메일을 입력해 주세요"
+        />
+        <Label htmlFor="email">닉네임</Label>
+        <Input
+          id="nickname"
+          name="nickname"
+          type="nickname"
+          placeholder="닉네임을 입력해 주세요"
+        />
+        <Label htmlFor="password">비밀번호</Label>
+        <Input
+          id="password"
+          name="password"
+          type="password"
+          placeholder="비밀번호를 입력해 주세요"
+        />
+        <Label htmlFor="password">비밀번호 확인</Label>
+        <Input
+          id="password"
+          name="password"
+          type="password"
+          placeholder="비밀번호를 다시 한 번 입력해 주세요"
+        />
+        <Button isable={false} text="회원가입" />
+        <EasyLogin />
+        <div className={styles.link}>
+          이미 회원이신가요?
+          <Link href="/login" className={styles.text_decoration}>
+            로그인하기
+          </Link>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default register;
